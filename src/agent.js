4d6dc8df526dd821d8bcfabe9c9d15f24e4a2783
Shibra Amin <shibraamin18@gmail.com>
2021-05-18 12:44:05 +0530

    Update agent.js

--- src/agent.js
+++ src/agent.js
@@ -3,7 +3,7 @@ import _superagent from 'superagent';
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
-const API_ROOT = 'hhttps://node-cicd-shibra.squareops.xyz/api';
+const API_ROOT = 'https://node-cicd-shibra.squareops.xyz/api';
 
 const encode = encodeURIComponent;
 const responseBody = res => res.body;
